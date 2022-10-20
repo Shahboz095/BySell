9abaeb390a84238a0068f4be7b9003e645310393
Shahboz095 <shahboz.17@mail.ru>
2022-10-20 18:08:13 +0500

    bug fixced and security

--- src/main/java/com/example/bysell/service/UserService.java
+++ src/main/java/com/example/bysell/service/UserService.java
@@ -1,7 +1,31 @@
 package com.example.bysell.service;
 
+import com.example.bysell.models.User;
+import com.example.bysell.models.enums.Role;
+import com.example.bysell.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
+@Slf4j
 public class UserService {
-     //
-    
+    //
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public boolean createUser(User user) {
+        String email = user.getEmail();
+        if (userRepository.findByEmail(user.getEmail()) != null)
+            return false;
+        user.setActive(true);
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        user.getRoles().add(Role.ROLE_USER);
+        log.info("Saving new User with email : {}", email);
+        return true;
+
+    }
+
 }
